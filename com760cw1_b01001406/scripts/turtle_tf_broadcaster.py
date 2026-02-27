348e757109b5f4be62863ab4c1ed6eac4e7aa5d4
richardson-j20 <richardson-j20@ulster.ac.uk>
2026-02-27 23:33:06 +0000

    Update turtle_tf_broadcaster.py

--- com760cw1_b01001406/scripts/turtle_tf_broadcaster.py
+++ com760cw1_b01001406/scripts/turtle_tf_broadcaster.py
@@ -1,39 +1,72 @@
 #!/usr/bin/env python3
+
 import rospy
-import tf_conversions
 import tf2_ros
-import geometry_msgs.msg
-import turtlesim.msg
+import tf_conversions
+from geometry_msgs.msg import TransformStamped
+from turtlesim.msg import Pose
 
-def handle_turtle_pose(msg, turtlename):
-    br = tf2_ros.TransformBroadcaster()
-    t = geometry_msgs.msg.TransformStamped()
 
-    t.header.stamp = rospy.Time.now()
-    t.header.frame_id = "world"
-    t.child_frame_id = turtlename
+class TurtleTFBroadcaster:
 
-    t.transform.translation.x = msg.x
-    t.transform.translation.y = msg.y
-    t.transform.translation.z = 0.0
+    def __init__(self):
 
-    q = tf_conversions.transformations.quaternion_from_euler(0, 0, msg.theta)
-    t.transform.rotation.x = q[0]
-    t.transform.rotation.y = q[1]
-    t.transform.rotation.z = q[2]
-    t.transform.rotation.w = q[3]
+        rospy.init_node("turtle_tf_broadcaster")
 
-    br.sendTransform(t)
+        # Get turtle name from launch file parameter
+        self.turtle_name = rospy.get_param("~turtle")
 
-if __name__ == '__main__':
-    rospy.init_node('turtle_tf_broadcaster')
+        # Create one broadcaster instance (not per callback)
+        self.br = tf2_ros.TransformBroadcaster()
 
-    turtle_name = rospy.get_param('~turtle')
+        # Subscribe to turtlesim pose
+        rospy.Subscriber(
+            f"/{self.turtle_name}/pose",
+            Pose,
+            self.pose_callback
+        )
 
-    rospy.Subscriber(f'/{turtle_name}/pose',
-                     turtlesim.msg.Pose,
-                     handle_turtle_pose,
-                     turtle_name)
+        rospy.loginfo(f"TF broadcaster started for {self.turtle_name}")
 
-    rospy.spin()
-    
+        rospy.spin()
+
+    # ----------------------------------------------------------
+
+    def pose_callback(self, msg):
+
+        t = TransformStamped()
+
+        # Time stamp
+        t.header.stamp = rospy.Time.now()
+
+        # Parent frame
+        t.header.frame_id = "world"
+
+        # Child frame
+        t.child_frame_id = self.turtle_name
+
+        # Position
+        t.transform.translation.x = msg.x
+        t.transform.translation.y = msg.y
+        t.transform.translation.z = 0.0
+
+        # Orientation (convert yaw to quaternion)
+        q = tf_conversions.transformations.quaternion_from_euler(
+            0,
+            0,
+            msg.theta
+        )
+
+        t.transform.rotation.x = q[0]
+        t.transform.rotation.y = q[1]
+        t.transform.rotation.z = q[2]
+        t.transform.rotation.w = q[3]
+
+        # Broadcast transform
+        self.br.sendTransform(t)
+
+if __name__ == "__main__":
+    try:
+        TurtleTFBroadcaster()
+    except rospy.ROSInterruptException:
+        pass
